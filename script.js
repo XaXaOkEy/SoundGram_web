497bca1ee430139bec9d794e7db5a1b1763cfdf0
SoundGram <seregavl2008@gmail.com>
2026-02-10 01:23:38 +0500

    сайт

---
+++ script.js
@@ -0,0 +1,13 @@
+// Анимация при скролле
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// 3D эффект для телефона удален
